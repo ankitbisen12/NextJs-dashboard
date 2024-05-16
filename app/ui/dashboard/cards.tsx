bba35b663dce8b35a178da7dc772011403151c88
Ankit Bisen <76652297+ankitbisen12@users.noreply.github.com>
2024-05-16 21:22:38 +0530

    Adding streaming logic,Suspense

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   BanknotesIcon,
   ClockIcon,
@@ -5,6 +6,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,19 +16,26 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    noOfCustomers,
+    noOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  
   return (
-    <>
+    <React.Fragment>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Total Invoices" value={noOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
-        value={numberOfCustomers}
+        value={noOfCustomers}
         type="customers"
-      /> */}
-    </>
+      />
+    </React.Fragment>
   );
 }
 
